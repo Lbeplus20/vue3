8fb5f8e62b9f3642fdcb3de6e10a95e4a3e6bc57
zwj <2718@qq,com>
2023-05-31 15:01:01 +0800

    初步完善

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/global.css'
+import router from "@/router";
+import store from "@/store";
+import request from "@/util/request";
 
 Vue.config.productionTip = false
+Vue.use(ElementUI,{size:"mini"})
+Vue.prototype.request=request
 
 new Vue({
   render: h => h(App),
+  router:router,
+  store:store,
 }).$mount('#app')
